8b3847f0c89bdfc6f0bc13b34d3cca6c1c05fac8
Martijn van Dijk <mhvdijk@gmail.com>
2016-03-09 13:31:44 +0100

    Delete wrong path

--- vvmCross/CodeAnalysis/MvvmCross.CodeAnalysis/Core/DiagnosticIds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MvvmCross.CodeAnalysis.Core
-{
-    public static class DiagnosticIds
-    {
-        public const string UseGenericBaseClassRuleId = "MVX1001";
-        public const string CommandWithCanExecuteWithoutCanExecuteChangedRuleId = "MVX1002";
-    }
-}
